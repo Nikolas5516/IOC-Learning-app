5a9160f818583cfbd65a7d503686928a9fc699aa
Nikolas5516 <nikolas.gavaz@stud.ubbcluj.ro>
2025-11-21 20:02:35 +0200

    Initial commit: backend + frontend

---
+++ src/test/java/com/ioc/learningapp/LearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioc.learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
